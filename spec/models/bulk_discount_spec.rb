58821e640e7fa6bc34bee8bb2189348342d03d63
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-01 14:41:51 -0700

    Test: validations and associations for bulk discount model, and the pertinent tests in merchant model

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ describe 'validations' do
+  it { should validate_presence_of :percentage_discount }
+  it { should validate_presence_of :quantity_treshold }
+ end
+
+ describe 'relationships' do
+  it { should belong_to :merchant }
+  #it { should have_many : }
+ end
+
+ describe 'instance methods' do
+ end
 end
