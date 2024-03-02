7df82b37bcf154155c3038a0a8b12ebbc8cd1fbb
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-02 00:24:10 -0700

    draft 1 user story5

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,6 +7,7 @@ class Invoice < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+  has_many :bulk_discounts, through: :merchants
 
   enum status: ["in progress", "cancelled", "completed"]
   
@@ -29,5 +30,28 @@ class Invoice < ApplicationRecord
   
   def total_revenue
     invoice_items.sum("unit_price * quantity")
+  end
+
+  def self.net_revenue
+    brute_revenue - discounts
+  end
+
+  def self.brute_revenue
+    total_revenue_in_cents = Invoice
+      .joins(:invoice_items)
+      .group("invoices.id")
+      .pluck(Arel.sql("sum(invoice_items.unit_price * invoice_items.quantity) as revenue"))
+      .sum
+
+    total_revenue_in_cents/100
+  end
+
+  def self.discounts
+    Invoice.joins(merchants: :bulk_discounts).joins(items: :invoice_items)
+      .select("merchants.id, bulk_discounts.*, items.*, invoice_items.*")
+      .where("merchants.id = items.merchant_id AND invoice_items.quantity >= bulk_discounts.quantity_treshold")
+      .group("invoices.id, bulk_discounts.id, items.id, invoice_items.id").pluck(Arel.sql("invoice_items.quantity * invoice_items.unit_price * (bulk_discounts.percentage_discount/100)")).sum
+    # require 'pry'; binding.pry
+    # Merchant.joins(items: :invoice_items).joins(:bulk_discounts)
   end
 end
