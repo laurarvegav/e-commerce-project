0dbb74529a80d9f7e4c49de8a53374159eea0b35
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-01 17:27:00 -0700

    Add should validate numericality of discount attributes

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe BulkDiscount, type: :model do
  describe 'validations' do
   it { should validate_presence_of :percentage_discount }
   it { should validate_presence_of :quantity_treshold }
+  it { should validate_numericality_of :percentage_discount }
+  it { should validate_numericality_of :quantity_treshold }
  end
 
  describe 'relationships' do
