215f37c1c01d0fda52c940025a4bd834919cbff9
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-01 17:31:45 -0700

    Feat: Create new and create actions for BulkDiscountsController

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -5,6 +5,25 @@ class BulkDiscountsController < ApplicationController
   end
 
   def show
-    require 'pry'; binding.pry
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = BulkDiscount.new(discount_merchant_params)
+    if bulk_discount.save
+      redirect_to merchant_bulk_discounts_path(merchant)
+    else 
+      redirect_to new_merchant_bulk_discount_path(merchant.id)
+      flash[:alert] =  "Error: #{error_message(bulk_discount.errors)}"
+    end
+  end
+
+  private
+  def discount_merchant_params
+    params.permit(:percentage_discount, :quantity_treshold, :merchant_id)
   end
 end
