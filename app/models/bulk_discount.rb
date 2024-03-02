2a15378fadf2be7eb9eff5af07ac84198a8c7800
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-01 17:27:18 -0700

    Add validates numericality of discount attributes

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,5 +2,8 @@ class BulkDiscount < ApplicationRecord
   validates_presence_of :percentage_discount,
                         :quantity_treshold
 
+  validates_numericality_of :percentage_discount,
+                        :quantity_treshold
+
   belongs_to :merchant
 end
