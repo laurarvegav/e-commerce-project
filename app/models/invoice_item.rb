af9b11a9c7b0a3fc0f7b0b830f25b2d632d0e4ce
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-03 11:45:50 -0700

    Create private method to default status to disabled at creation

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -11,21 +11,4 @@ class InvoiceItem < ApplicationRecord
   def unit_price_to_dollars
     unit_price/100.00
   end
-
-  def self.discounts
-    joins(item: { merchant: :bulk_discounts })
-    .select(
-      "invoice_items.item_id,
-       invoice_items.quantity,
-       invoice_items.unit_price,
-       bulk_discounts.quantity_threshold,
-       bulk_discounts.percentage_discount,
-       CASE
-         WHEN invoice_items.quantity >= bulk_discounts.quantity_treshold THEN
-           invoice_items.quantity * invoice_items.unit_price * (1 - bulk_discounts.percentage_discount / 100)
-         ELSE
-           invoice_items.quantity * invoice_items.unit_price
-       END AS discounted_price"
-    )
-  end
 end
