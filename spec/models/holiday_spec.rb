9f13019bee0048fd681add7e8ca2b0adcbe52a04
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-03 00:15:29 -0700

    Test: Add test for Initialize method for Holiday Class

---
+++ spec/models/holiday_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Holiday, type: :model do
+  describe 'initialize' do
+    it "initializes holiday instance with attributes" do
+      holiday = Holiday.new({date: "2024-03-03", name: "President's Day"})
+
+      expect(holiday).to be_a(Holiday)
+      expect(holiday.name).to eq("President's Day")
+      expect(holiday.date).to eq("2024-03-03")
+    end
+  end
+end
