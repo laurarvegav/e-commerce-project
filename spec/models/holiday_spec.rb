4bd1691afd9b87b971491f58bf7db2b2cac1f17a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-03 11:40:49 -0700

    Test: Instance method testing

--- spec/models/holiday_spec.rb
+++ spec/models/holiday_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Holiday, type: :model do
   describe 'initialize' do
     it "initializes holiday instance with attributes" do
-      holiday = Holiday.new({date: "2024-03-03", name: "President's Day"})
+      holiday = Holiday.new({"date"=> "2024-03-03", "name"=> "President's Day"})
 
       expect(holiday).to be_a(Holiday)
       expect(holiday.name).to eq("President's Day")
