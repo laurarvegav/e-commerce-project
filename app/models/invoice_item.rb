144e3b787b391411fb0c058c14504e9b06399185
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-05 17:53:01 -0700

    Feat: add brute_revenue, net_revenue and disct_to_apply methods of InvoiceItem Model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -11,4 +11,16 @@ class InvoiceItem < ApplicationRecord
   def unit_price_to_dollars
     unit_price/100.00
   end
+
+  def brute_revenue
+    quantity * unit_price_to_dollars
+  end
+
+  def net_revenue 
+     brute_revenue * (1 - disct_to_apply).round(2)
+  end
+
+  def disct_to_apply
+    item.merchant.eligible_discount(quantity)/100.0
+  end
 end
